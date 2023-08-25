eabec6e312ff6b9ae86210ada5d452452aaf404d
Daniyal ali <daniyalali12568@gmail.com>
2023-08-25 16:58:56 +0500

    practicenavbar

---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.css";
+import './navbar.css'
+const Navbar = () => {
+  <>
+    <nav>
+      <div className="mainNavbar">
+        <div>Home</div>
+        <div>About</div>
+        <div>Contact</div>
+        <div>Project</div>
+      </div>
+    </nav>
+  </>;
+};
+export default Navbar;
